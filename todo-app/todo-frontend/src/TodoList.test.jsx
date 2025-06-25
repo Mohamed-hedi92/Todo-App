2dfac342f64b11315d8efce093376b0b6db3147c
bou <mohamed.bouasida@iso-gruppe.com>
2025-06-25 09:31:00 +0200

    repository und jenkinsfile anpassen

--- todo-app/todo-frontend/src/TodoList.test.jsx
+++ todo-app/todo-frontend/src/TodoList.test.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import TodoList from "../TodoList";
+import TodoList from "./TodoList";
 import axios from "axios";
 
 jest.mock("axios");
