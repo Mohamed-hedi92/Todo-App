a1437d140009826575787123bd0fca8262e63973
bou <mohamed.bouasida@iso-gruppe.com>
2025-07-01 12:20:04 +0200

    jest.config anpassen

--- todo-app/todo-frontend/jest.config.js
+++ todo-app/todo-frontend/jest.config.js
@@ -2,15 +2,19 @@ module.exports = {
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
   },
+
   testEnvironment: 'jsdom',
+
   transform: {
     "^.+\\.[jt]sx?$": "babel-jest"
   },
-testMatch: [
-  "**/?(*.)+(spec|test).[jt]s?(x)"
-],
-testPathIgnorePatterns: [
-  "/node_modules/",
-  "/e2e/"
-]
+
+  testMatch: [
+    "**/src/**/?(*.)+(spec|test).[jt]s?(x)"
+  ],
+
+  testPathIgnorePatterns: [
+    "/node_modules/",
+    "/e2e/"
+  ]
 };
